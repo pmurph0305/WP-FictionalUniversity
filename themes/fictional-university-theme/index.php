350e067174c78c483da331564cb6bf9c3bbbdb49
unknown <pmurph0305@gmail.com>
2019-08-27 16:56:42 -0400

    php function basics

--- themes/fictional-university-theme/index.php
+++ themes/fictional-university-theme/index.php
@@ -1,7 +1,9 @@
 <?php
-    function myFirstFunction() {
-        echo '<p>Hello this is a paragraph.</p>';
+    function greet($name, $color) {
+        echo "<p>Hey, my name is $name and my favorite color is $color.</p>";
     }
-    myFirstFunction();
-    myFirstFunction();
+    greet('Patrick', 'orange');
+    greet('Jessica', 'blue');
 ?>
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description')?></p>
