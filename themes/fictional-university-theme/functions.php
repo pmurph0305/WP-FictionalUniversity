80f568512c598314982740dcb5c5d1b64bf5cbd1
unknown <pmurph0305@gmail.com>
2019-08-28 13:06:36 -0400

    covert static html example to theme

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -1,5 +1,8 @@
 <?php
     function universityFiles() {
+        wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+        wp_enqueue_style('custom_google_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+        wp_enqueue_style('font_awesome_icons', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
         wp_enqueue_style('university_main_styles', get_stylesheet_uri());
     }
 
