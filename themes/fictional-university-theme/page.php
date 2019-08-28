ffc71e2c8ce236709c276a1aec8d3d1533386fae
unknown <pmurph0305@gmail.com>
2019-08-28 14:11:31 -0400

    Parent & child pages

--- themes/fictional-university-theme/page.php
+++ themes/fictional-university-theme/page.php
@@ -16,10 +16,24 @@
         </div>
 
         <div class="container container--narrow page-section">
-
-            <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-            </div>
+            <?php 
+                $parentId = wp_get_post_parent_id(get_the_ID());
+                if ($parentId) {
+            ?>
+                <div class="metabox metabox--position-up metabox--with-home-link">
+                    <p><a class="metabox__blog-home-link" 
+                            href="<?php echo get_permalink($parentId); ?>">
+                            <i class="fa fa-home" aria-hidden="true"></i> 
+                            Back to 
+                            <?php echo get_the_title($parentId); ?>
+                        </a> <span class="metabox__main"><?php the_title(); ?></span></p>
+                </div>
+            <?php 
+                }
+            ?>
+            <!-- <div class="metabox metabox--position-up metabox--with-home-link">
+            <p><a class="metabox__blog-home-link" href="<?php echo site_url('/about-us'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+            </div> -->
             
             <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
