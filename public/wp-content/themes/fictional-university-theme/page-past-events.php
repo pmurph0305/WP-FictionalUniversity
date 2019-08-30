dad54066d8a574a93e5c3ffb07d7a8fb2b4b6733
unknown <pmurph0305@gmail.com>
2019-08-30 14:28:14 -0400

    get_template_part() function to reduce duplicate code

--- public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -7,38 +7,27 @@
 ?>
 
 <div class="container container--narrow page-section">
-    <?php
-        $today = date('Ymd');
-        $pastEventPosts = new WP_Query(array(
-        'paged' => get_query_var('paged', 1),
-        'post_type' => 'event',
-        'orderby' => 'meta_value_num',
-        'meta_key' => 'event_date',
-        'meta_query' => array(
-            array(
-            'key' => 'event_date',
-            'value' => $today,
-            'compare' => '<',
-            'type' => 'numeric',
-            )
+  <?php
+    $today = date('Ymd');
+    $pastEventPosts = new WP_Query(array(
+    'paged' => get_query_var('paged', 1),
+    'post_type' => 'event',
+    'orderby' => 'meta_value_num',
+    'meta_key' => 'event_date',
+    'meta_query' => array(
+        array(
+        'key' => 'event_date',
+        'value' => $today,
+        'compare' => '<',
+        'type' => 'numeric',
         )
-        ));
+    )
+    ));
 
-        while($pastEventPosts->have_posts()) {
-            $pastEventPosts->the_post();
-            $eventDate = new DateTime(get_field('event_date'));
-    ?>
-        <div class="event-summary">
-            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month"><?php echo $eventDate->format('M')?></span>
-                <span class="event-summary__day"><?php echo $eventDate->format('d')?></span>  
-            </a>
-            <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-            </div>
-        </div>
-  <?php  
+    while($pastEventPosts->have_posts()) {
+        $pastEventPosts->the_post();
+        $eventDate = new DateTime(get_field('event_date'));
+        get_template_part('template-parts/content-event');
     }
     echo paginate_links(array(
         'total' => $pastEventPosts->max_num_pages,
