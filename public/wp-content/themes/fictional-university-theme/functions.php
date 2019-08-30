8f6492f1bc4d0bf8b56c60a8b4ab3d3105935ea2
unknown <pmurph0305@gmail.com>
2019-08-30 19:00:11 -0400

    Add dev-only google api key

--- public/wp-content/themes/fictional-university-theme/functions.php
+++ public/wp-content/themes/fictional-university-theme/functions.php
@@ -32,7 +32,7 @@
 
     function universityFiles() {
         // cache fix from quick note using microtime() for versions for styles & bundled scripts.
-        wp_enqueue_script('googleMap', "//maps.googleapis.com/maps/api/js?key=".GOOGLE_API_KEY, NULL, microtime(), true);
+        wp_enqueue_script('googleMap', "//maps.googleapis.com/maps/api/js?key=AIzaSyDuixh5dYEw4z1yhUuNLKRncySSqpTuO30", NULL, microtime(), true);
         wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
         wp_enqueue_style('custom_google_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
         wp_enqueue_style('font_awesome_icons', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -81,7 +81,7 @@
 
     // Example if you have a valid Google API Key.
     function universityMapKey($api) {
-    $api['key'] = GOOGLE_API_KEY;
+    $api['key'] = 'AIzaSyDuixh5dYEw4z1yhUuNLKRncySSqpTuO30';
     return $api;
     }
     add_filter('acf/fields/google_map/api', 'universityMapKey');
