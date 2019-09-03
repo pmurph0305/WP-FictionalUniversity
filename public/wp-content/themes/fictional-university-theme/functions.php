39ebf48129f7bda6d68ee6c0c3cc4423aee6eaff
unknown <pmurph0305@gmail.com>
2019-09-03 12:42:45 -0400

    Live search with wordpress REST api

--- public/wp-content/themes/fictional-university-theme/functions.php
+++ public/wp-content/themes/fictional-university-theme/functions.php
@@ -37,6 +37,9 @@
         wp_enqueue_style('custom_google_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
         wp_enqueue_style('font_awesome_icons', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
         wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
+        wp_localize_script('main-university-js', 'universityData', array(
+            'rootUrl' =>  get_site_url(),
+        ));
     }
 
     add_action('wp_enqueue_scripts', 'universityFiles');
--- public/wp-content/themes/fictional-university-theme/js/modules/LiveSearch.js
+++ public/wp-content/themes/fictional-university-theme/js/modules/LiveSearch.js
@@ -3,6 +3,8 @@ import $ from 'jquery';
 class LiveSearch {
   // 1. Describe and create/initiate our object.
   constructor() {
+    this.addSearchHTML();
+
     this.closeButton = $(".search-overlay__close");
     this.openButton = $(".js-search-trigger");
   
@@ -29,10 +31,63 @@ class LiveSearch {
 
 
   // 3. methods
+  addSearchHTML() {
+    $("body").append(`
+    <div class="search-overlay">
+      <div class="search-overlay__top">
+        <div class="container">
+          <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+          <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+          <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+        </div>
+      </div>
+      <div class="container">
+        <div id="search-overlay__results">
+        </div>
+      </div>
+    </div>
+    `)
+  }
+
   closeOverlay() {
     this.isOverlayOpen = false;
     this.searchOverlay.removeClass('search-overlay--active');
     $("body").removeClass("body-no-scroll");
+  }
+
+  getSearchResults() {
+    $.when(
+      $.getJSON(universityData.rootUrl + "/wp-json/wp/v2/posts?search=" + this.searchField.val()), 
+      $.getJSON(universityData.rootUrl + "/wp-json/wp/v2/pages?search=" + this.searchField.val())
+    ).then((posts, pages) => {
+      let results = posts[0].concat(pages[0]);
+        this.searchResultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Info</h2>
+        ${results.length ? '<ul class="link-list min-list">' : '<p>No results found.</p>'}
+          ${results.map(result => `<li><a href="${result.link}">${result.title.rendered}</a></li>`).join('')}
+          ${results.length ? '</ul>' : ''}
+      `)
+      this.isLoadingSpinnerDisplayed = false;
+    }, () => {
+      this.searchResultsDiv.html(`<p>Unexpected error, please try again.</p>`);
+    })
+  }
+
+  handleSearchTyping(e) {
+    if (this.searchField.val() !== this.previousValue) {
+      clearTimeout(this.searchTimeout);
+      if (this.searchField.val()) {
+        if (!this.isLoadingSpinnerDisplayed) {
+          this.isLoadingSpinnerDisplayed = true;
+          this.searchResultsDiv.html('<div class="spinner-loader"></div>');
+        }
+        this.searchTimeout = setTimeout(this.getSearchResults.bind(this), 500);
+      } else {
+        this.isLoadingSpinnerDisplayed = false;
+        this.searchResultsDiv.html('');
+      }
+    }
+    this.previousValue = this.searchField.val();
   }
   
   keyPressDispatcher(e) {
@@ -45,32 +100,11 @@ class LiveSearch {
 
   openOverlay() {
     this.isOverlayOpen = true;
+    this.searchField.val("");
     this.searchOverlay.addClass('search-overlay--active');
     $("body").addClass("body-no-scroll");
+    setTimeout(() => this.searchField.focus(), 301);
   }
-
-  handleSearchTyping(e) {
-    if (this.searchField.val() !== this.previousValue) {
-      clearTimeout(this.searchTimeout);
-      if (this.searchField.val()) {
-        if (!this.isLoadingSpinnerDisplayed) {
-          this.isLoadingSpinnerDisplayed = true;
-          this.searchResultsDiv.html('<div class="spinner-loader"></div>');
-        }
-        this.searchTimeout = setTimeout(this.getSearchResults.bind(this), 1000);
-      } else {
-        this.isLoadingSpinnerDisplayed = false;
-        this.searchResultsDiv.html('');
-      }
-    }
-    this.previousValue = this.searchField.val();
-  }
-
-  getSearchResults() {
-    this.isLoadingSpinnerDisplayed = false;
-    this.searchResultsDiv.html("example")
-  }
-
 } 
 
 export default LiveSearch;
