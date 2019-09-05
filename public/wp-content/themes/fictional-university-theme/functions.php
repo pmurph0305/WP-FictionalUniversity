d1b8b03e1a08f66df5c17347e82ccb68eb9dd7e6
unknown <pmurph0305@gmail.com>
2019-09-05 15:45:56 -0400

    Files..

--- public/wp-content/themes/fictional-university-theme/functions.php
+++ public/wp-content/themes/fictional-university-theme/functions.php
@@ -18,6 +18,9 @@
     // customize login header, url, logo
     require get_theme_file_path('/includes/login-customize.php');
 
+    // make notes route private
+    require get_theme_file_path('/includes/notes-make-private.php');
+
     // Example if you have a valid Google API Key.
     function universityMapKey($api) {
     $api['key'] = 'AIzaSyDuixh5dYEw4z1yhUuNLKRncySSqpTuO30';
@@ -35,22 +38,4 @@
         ));
     }
     add_action('rest_api_init', 'universityCustomizeREST');
-    
-
-
-    // Force note posts to be private.
-    function forceNotesToBePrivate($data, $postarr) {
-        if ($data['post_type'] == 'note') {
-            if (count_user_posts(get_current_user_id(), 'note') > 4 AND !$postarr['ID']) {
-                die('You have reached your maximum note limit.');
-            }
-            $data['post_content'] = sanitize_textarea_field($data['post_content']);
-            $data['post_title'] = sanitize_text_field($data['post_title']);
-        }
-        if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
-            $data['post_status'] = 'private';
-        }
-        return $data;
-    }
-    add_filter('wp_insert_post_data', 'forceNotesToBePrivate', 10, 2);
 ?>
